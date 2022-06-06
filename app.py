0651cc2cca9d55ebb6c4be6b6fac3d668a9712ad
Viran Ranaraja <venuraka.20191253@iit.ac.lk>
2022-06-06 14:51:36 +0530

    bot present servers

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ winningConditions = [
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    servernum = str(len(client.guilds))
+    print('Currently in ' + servernum)
 
 
 @client.command()
